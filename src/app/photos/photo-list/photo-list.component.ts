4cc6d58a5180991c484ea75c3c6d73ec7e25745b
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-10-14 00:05:16 -0300

    add loadMore Button

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
-import { Photo } from '../photo/photo';
 import { debounceTime } from 'rxjs/operators';
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -14,13 +15,18 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
   
   constructor(
-    private activatedRoute: ActivatedRoute 
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
   ){ }
   
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
 
     this.debounce
@@ -30,6 +36,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+        .subscribe( photos => {
+          this.photos = this.photos.concat(photos)
+          if(!photos.length){
+            this.hasMore = false;
+          }
+        })
   }
 
 }
