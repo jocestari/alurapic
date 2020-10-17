efaa5c3428338650e7098d66d2319b8e28ae87a7
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-10-17 01:12:59 -0300

    add signUp

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.mpdule';
 import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
 
 
 @NgModule({
     declarations:[
-        SignInComponent
+        SignInComponent,
+        SignUpComponent
     ],
     imports: [ 
         ReactiveFormsModule,
         CommonModule,
         VMessageModule,
-        RouterModule
+        RouterModule,
+        FormsModule
      ]
 })
 export class HomeModule{ }
