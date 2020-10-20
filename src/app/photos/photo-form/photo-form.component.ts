90250c3a2fb1a717f985aff05ad142c9f6601b5a
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-10-20 19:36:16 -0300

    Photo Preview

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -11,6 +11,7 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
+  preview: string;
 
   constructor(
     private formBuilder: FormBuilder, 
@@ -33,6 +34,14 @@ export class PhotoFormComponent implements OnInit {
       .upload(description, allowComments, this.file)
       .subscribe(() => this.router.navigate(['']))
     
+  }
+
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => 
+        this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 }
 
