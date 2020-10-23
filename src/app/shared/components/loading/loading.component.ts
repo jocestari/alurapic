12234d0494065cab44e39183276b5e23c227d00b
James de Oliveira Cestari <james.cestari@mpt.mp.br>
2020-10-23 17:00:16 -0300

    add progress bar

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LoadingType } from './loading-type';
+import { LoadingService } from './loading.service';
 
 @Component({
     selector: 'ap-loading',
     templateUrl: './loading.component.html',
     styleUrls: ['./loading.component.css']
 })
-export class LoadingComponent { }
+export class LoadingComponent implements OnInit {
+
+    loading$: Observable<string>;
+
+    constructor(
+        private loadingService: LoadingService
+    ) { }
+
+    ngOnInit(): void {
+        this.loading$ = this.loadingService
+        .getLoading()
+        .pipe(map(loadingType => loadingType.valueOf()))
+    }
+}
